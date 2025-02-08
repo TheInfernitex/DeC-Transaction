318c4ecc348b0ce298f9f84d248ba4697baca2b9
TheInfernitex <agarwalparthsplendid@gmail.com>
2025-02-08 22:16:19 +0530

    Add: Thirdweb Client id

--- frontend/src/components/ClientThirdWebProvider.tsx
+++ frontend/src/components/ClientThirdWebProvider.tsx
@@ -6,6 +6,7 @@ import { Sepolia } from "@thirdweb-dev/chains";
 const ClientThirdWebProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThirdwebProvider
+    clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID} 
       activeChain={Sepolia} // Default to Sepolia, can be dynamic later
       supportedWallets={[metamaskWallet(), walletConnect(), coinbaseWallet()]}
     >
